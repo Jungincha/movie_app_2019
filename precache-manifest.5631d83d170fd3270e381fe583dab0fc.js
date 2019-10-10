e40c449d5b8c7972cd4ae5a4572355ae708845dc
Jungincha <jungincha923@gmail.com>
2019-10-10 22:10:44 +0900

    Updates

---
+++ precache-manifest.5631d83d170fd3270e381fe583dab0fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c287e5839a91ecef953649f6978058",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "7f84ed7c0a86201fa463",
+    "url": "/movie_app_2019/static/css/main.0b600a33.chunk.css"
+  },
+  {
+    "revision": "9c63e566ee8166e9431f",
+    "url": "/movie_app_2019/static/js/2.6d27e71f.chunk.js"
+  },
+  {
+    "revision": "7f84ed7c0a86201fa463",
+    "url": "/movie_app_2019/static/js/main.c67d435e.chunk.js"
+  },
+  {
+    "revision": "420037a5e08d7d6458d7",
+    "url": "/movie_app_2019/static/js/runtime-main.6a961451.js"
+  }
+]);
